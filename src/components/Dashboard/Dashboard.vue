38b0b323972bbc1ff4c50390e4076194692b44a5
Joe-A11-RM <yousefsherif7500@gmail.com>
2025-08-31 15:53:24 +0300

    Feat(Layout)

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,7 +1,15 @@
 <script setup></script>
 
 <template>
-  <div class="dashboard-layout">aa</div>
+  <div class="dashboard-layout">
+    <v-row>
+      <!-- First column -->
+      <v-col cols="12" lg="8" md="6" order-lg="1"> a </v-col>
+
+      <!-- Second column -->
+      <v-col cols="12" lg="4" md="6" order-lg="2"> b </v-col>
+    </v-row>
+  </div>
 </template>
 
 <style scoped>
